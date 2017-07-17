5e92ddcfc22d31033d29860f88d33af73fcd7723
Checkmobi <support@checkmobi.com>
2017-07-17 11:42:37 +0300

    Remove unused method

--- sample/src/main/java/com/checkmobi/Utils.java
+++ sample/src/main/java/com/checkmobi/Utils.java
@@ -6,16 +6,6 @@ import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.X509TrustManager;
-
 class Utils
 {
     public static final int LOLLIPOP = 21;
@@ -55,35 +45,6 @@ class Utils
         ConnectivityManager cm = (ConnectivityManager) ct.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm.getActiveNetworkInfo();
         return ni != null;
-    }
-
-    public static void TrustInvalidSslCertificates()
-    {
-        try
-        {
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier()
-            {
-                public boolean verify(String hostname, SSLSession session)
-                {
-                    return true;
-                }});
-
-                SSLContext context = SSLContext.getInstance("TLS");
-                context.init(null, new X509TrustManager[]{new X509TrustManager()
-                {
-                    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
-                    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
-                    public X509Certificate[] getAcceptedIssuers()
-                    {
-                        return new X509Certificate[0];
-                    }}}, new SecureRandom());
-
-                    HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
     }
 
     public static boolean isCompatible(int apiLevel)
